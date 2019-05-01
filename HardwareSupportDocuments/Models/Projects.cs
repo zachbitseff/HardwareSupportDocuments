0b67b8ccc3965478c10637472fae4d2831846725
Zachary Bitseff <zachbitseff@gmail.com>
2019-04-30 20:22:36 -0700

    Created Projects Class. 5/5 fields made.

---
+++ HardwareSupportDocuments/Models/Projects.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HardwareSupportDocuments.Models
+{
+    public class Projects
+    {
+
+        [Key]
+        public int ProjectID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+
+        // Navigation properties
+        //public virtual ICollection<FaultRecoveryLog> FaultLog { get; set; }
+        //public virtual ICollection<FunctionalTestingLog> FunctionalTestLog { get; set; }
+
+    }
+}
