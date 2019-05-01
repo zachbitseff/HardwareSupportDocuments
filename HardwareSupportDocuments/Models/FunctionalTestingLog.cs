4d3ce5b9624da0dbab12e104c002f5a7776845b7
Zachary Bitseff <zachbitseff@gmail.com>
2019-04-30 21:01:41 -0700

    Created FunctionalTestingLog class.  13/13 fields made.

---
+++ HardwareSupportDocuments/Models/FunctionalTestingLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HardwareSupportDocuments.Models
+{
+    public class FunctionalTestingLog
+    {
+        // Foreign Key
+        public int ProjectID { get; set; }
+        public virtual Projects Project { get; set; }
+
+        [Key]
+        public int FunctionalTestingLogID { get; set; }
+
+        public string Login { get; set; }
+
+        public DateTime Date { get; set; }
+        public string TestName { get; set; }
+
+        public string CurrentStateProblemDescription { get; set; }
+
+        public string ProposedSolution { get; set; }
+
+        public string ChangesMadeDuringTest { get; set; }
+
+        public string Observations { get; set; }
+
+        public string RecommendedChanges { get; set; }
+
+        public Boolean ChangeApproved { get; set; }
+
+        public string Hyperlinks { get; set; }
+
+        public string Results { get; set; }
+    }
+}
